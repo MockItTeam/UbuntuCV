985a1fa414ce1555c2a0af21d03e86ed653a73d5
mapfap <mapfaps@gmail.com>
2016-02-16 01:22:55 +0700

    Fix typo

--- opencv2_4_10.sh
+++ opencv2_4_10.sh
@@ -1,7 +1,7 @@
 # v1 by mapfap
 
 echo "Setup Pre-Dependenices"
-udo apt-get update
+sudo apt-get update
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev unzip qt-sdk
 echo "Done Pre-Step"
 
